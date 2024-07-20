138f1be0d30d3c12415a85431cc3794bb6b43350
Luis Moreno <luiggyss@gmail.com>
2024-07-19 21:44:10 -0400

    Agregue lo faltante

---
+++ js/Cl_edad.js
@@ -0,0 +1,14 @@
+export default class Cl_edad{
+    constructor(edad){
+        this._edad=edad;
+     }
+
+     setEdad(e){
+        this._edad=+e;
+     }
+
+     getEdad(){
+        return this._edad;
+     }
+
+}
